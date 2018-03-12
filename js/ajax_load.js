c9c8f8f0a7531c07f8458936e6f7d3edc8d3669f
Unknown <ponxosio@gmail.com>
2018-03-12 11:41:03 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ js/ajax_load.js
@@ -0,0 +1,22 @@
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                return allText;
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+function load_html(divId, htmlFilePath) {
+     text = readTextFile("file://" + htmlFilePath)
+     document.getElementById(divId).innerHTML= $.parseHTML(text);
+}
